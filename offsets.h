be3535c1d484f1699b2ad884e96f74c1a4d0f7ec
sebwett-eng <sebwett@gmail.com>
2026-01-27 14:49:33 -0500

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,104 +1,120 @@
 #pragma once
 #include <cstdint>
+#include <unordered_map>
+#include <fstream>
 
 // Current Patch: v39.30 - Build 2
-// Last Updated: Jan 27 2026 - 12:20 PM
 
 namespace offsets {
 
     // ===== Globals =====
-    inline uintptr_t UWorld = 0x178685D8; 
-    inline uintptr_t UWorldXorKey = 0xFFFFFFFF30B9BBF9; 
-    inline uintptr_t UWorldXorRotationCount = 0; 
-    inline uintptr_t GNames = 0x17A50D00; 
-    inline uintptr_t GEngine = 0x17869ED8; 
-    inline uintptr_t GObjects = 0x1785AFB0; 
-    inline uintptr_t GObjectsCount = 0x1785AFB8; 
-    inline uintptr_t GObjectsXorKey = 0xFFFFFFFFCCF54342ULL;
-    inline uintptr_t GObjectsCountXorKey = 0x75DB1A01;
-    inline uintptr_t StaticFindObject = 0x73AC13; 
-    inline uintptr_t StaticLoadObject = 0x960153;
+    uintptr_t UWorld = 0x178685D8; // updated: 0x178C37A8 -> 0x178685D8
+    uintptr_t UWorldXorKey = 0xFFFFFFFF30B9BBF9; // updated: 0xFFFFFFFFD599D092ULL -> 0xFFFFFFFF30B9BBF9
+    uintptr_t UWorldXorRotationCount = 0; // (simple exponent : no ror8 this update)
+    uintptr_t GNames = 0x1777E080; // updated: 0x17A50D00 -> 0x1777E080
+    uintptr_t GEngine = 0x17869ED8; // updated: 0x178C50A8 -> 0x17869ED8
+    uintptr_t GObjects = 0x1785AFB0; // updated: 0x17B2DC58 -> 0x1785AFB0
+    uintptr_t GObjectsCount = 0x1785AFB8; // updated: 0x17560ED4 -> 0x1785AFB8
+    uintptr_t GObjectsXorKey = 0xFFFFFFFFCCF54342ULL;
+    uintptr_t GObjectsCountXorKey = 0x75DB1A01;
+    uintptr_t StaticFindObject = 0x73AC13; // updated: 0x71B76C -> 0x73AC13
+    uintptr_t StaticLoadObject = 0x960153;
 
-    inline uintptr_t ProcessEvent = 0x86FAA; 
-    inline uintptr_t ProcessEventIndex = 71;
-    inline uintptr_t DrawTransition = 0x513D88;
-    inline uintptr_t BoneMatrix = 0x55B004;
+    uintptr_t ProcessEvent = 0x86FAA; // updated: 0x63B7E -> 0x86FAA
+    uintptr_t ProcessEventIndex = 71;
+    uintptr_t DrawTransition = 0x513D88;
+    uintptr_t BoneMatrix = 0x55B004;
 
     // ===== Player / Controller =====
-    inline uintptr_t LocalPlayers = 0x38;
-    inline uintptr_t PlayerController = 0x30;
-    inline uintptr_t PlayerCameraManager = 0x368; 
-    inline uintptr_t AcknowledgedPawn = 0x358;
-    inline uintptr_t PlayerState = 0x2D0;
-    inline uintptr_t MyHud = 0x360;
-    inline uintptr_t TeamIndex = 0x11B1;
-    inline uintptr_t SquadId = 0x1344;
-    inline uintptr_t bIsDying = 0x728;
-    inline uintptr_t bIsDBNO = 0x841;
-    inline uintptr_t bIsABot = 0x2BA;
+    uintptr_t LocalPlayers = 0x38;
+    uintptr_t PlayerController = 0x30;
+    uintptr_t PlayerCameraManager = 0x360; // updated: 0x368 -> 0x360
+    uintptr_t AcknowledgedPawn = 0x358;
+    uintptr_t PlayerState = 0x2D0;
+    uintptr_t NetConnection = 0x528;
+    uintptr_t TeamIndex = 0x11B1;
+    uintptr_t bIsDying = 0x728;
+    uintptr_t bIsDBNO = 0x841;
+    uintptr_t bIsABot = 0x2BA;
 
-    inline uintptr_t Platform = 0x440;
-    inline uintptr_t TargetedFortPawn = 0x1830;
-    inline uintptr_t KillScore = 0x11C8;
-    inline uintptr_t SeasonLevelUIDisplay = 0x11C4;
-    inline uintptr_t RankedProgress = 0xD8;
-    inline uintptr_t PlayerAimOffset = 0x2BD0;
-    inline uintptr_t Username = 0xA00;
+    uintptr_t Platform = 0x440;
+    uintptr_t TargetedFortPawn = 0x1830;
+    uintptr_t KillScore = 0x11C8;
+    uintptr_t RebootCount = 0x1894;
+    uintptr_t PlayerName = 0xA08;
+    uintptr_t PlayerNamePrivate = 0x348;
+    uintptr_t RankedProgress = 0xD8;
+    uintptr_t PlayerAimOffset = 0x2BD0;
 
     // ===== World =====
-    inline uintptr_t OwningGameInstance = 0x248; 
-    inline uintptr_t GameState = 0x1D0; 
-    inline uintptr_t PlayerArray = 0x2C8;
-    inline uintptr_t PersistentLevel = 0x40;
-    inline uintptr_t Levels = 0x1E8; 
-    inline uintptr_t OverlappingBuildings = 0x1D48;
+    uintptr_t OwningGameInstance = 0x248; // updated: 0x240 -> 0x248
+    uintptr_t GameState = 0x1D0; // updated: 0x1C8 -> 0x1D0
+    uintptr_t PlayerArray = 0x2C8;
+    uintptr_t WorldSettings = 0x2B8;
+    uintptr_t WorldGravityZ = 0x330;
+    uintptr_t WorldToMeters = 0x320;
+
+    uintptr_t PersistentLevel = 0x40;
+    uintptr_t Levels = 0x1E0;
 
     // ===== Actor / Pawn =====
-    inline uintptr_t RootComponent = 0x1B0;
-    inline uintptr_t PawnPrivate = 0x328;
-    inline uintptr_t BoundScale = 0x310;
+    uintptr_t AActor = 0x208;
+    uintptr_t RootComponent = 0x1B0;
+    uintptr_t PawnPrivate = 0x328;
+    uintptr_t MoveIgnoreActors = 0x348;
+    uintptr_t SuperField = 0x40;
 
     // ===== Mesh / Components =====
-    inline uintptr_t Mesh = 0x330;
-    inline uintptr_t BoneArray = 0x5F0;
-    inline uintptr_t BoneCache = 0x600; 
-    inline uintptr_t ComponentToWorld = 0x1E0;
-    inline uintptr_t RelativeLocation = 0x140;
-    inline uintptr_t RelativeRotation = 0x158;
-    inline uintptr_t ComponentVelocity = 0x188;
-    inline uintptr_t AdditionalAimOffset = 0x2BA0;
-    inline uintptr_t LastRenderTime = 0x32C; // BoundScale + 0x1C
-    inline uintptr_t CustomDepthComponent = 0x5040;
+    uintptr_t Mesh = 0x330;
+    uintptr_t BoneArray = 0x5F0;
+    uintptr_t BoneCache = 0x600; // updated: 0x5F8 -> 0x600
+    uintptr_t MeshDeformerInstances = 0x5C0;
+
+    uintptr_t ComponentToWorld = 0x1E0;
+    uintptr_t RelativeLocation = 0x140;
+    uintptr_t RelativeRotation = 0x158;
+    uintptr_t RelativeScale3D = 0x170;
+    uintptr_t ComponentVelocity = 0x188;
+    uintptr_t AdditionalAimOffset = 0x2BA0;
+    uintptr_t LastRenderTime = 0x328;
+    uintptr_t LocationUnderReticle = 0x2A50;
 
     // ===== Camera =====
-    inline uintptr_t CameraLocation = 0x180; 
-    inline uintptr_t CameraRotation = 0x170; 
-    inline uintptr_t CameraFOV = 0x740; // FOV_Camera
-    inline uintptr_t ViewState = 0xD0;
+    uintptr_t CameraLocation = 0x170; // updated: 0x180 -> 0x170
+    uintptr_t CameraRotation = 0x180; // updated: 0x190 -> 0x180
+    uintptr_t CameraFOV = 0x3B4;
+
+    // ===== Vehicles =====
+    uintptr_t CurrentVehicle = 0x2C58;
 
     // ===== Weapons =====
-    inline uintptr_t CurrentWeapon = 0x990;
-    inline uintptr_t WeaponData = 0x688; 
-    inline uintptr_t WeaponList = 0x9C8;
-    inline uintptr_t AmmoCount = 0x144C;
-    inline uintptr_t bIsReloadingWeapon = 0x3B9;
-    inline uintptr_t CurrentReloadDuration = 0x1438;
-    inline uintptr_t WeaponRecoilOffset = 0x2C10;
-    inline uintptr_t WeaponCoreAnimation = 0x19B8;
-    inline uintptr_t TriggerType = 0x29C;
+    uintptr_t CurrentWeapon = 0x990;
+    uintptr_t WeaponData = 0x5A0; // updated: 0x600 -> 0x5A0
+    uintptr_t WeaponOffsetCorrection = 0x2C00;
+    uintptr_t AmmoCount = 0x14FC; // updated: 0x147C -> 0x14FC
+    uintptr_t bIsReloadingWeapon = 0x3D1;
+    uintptr_t ReloadAnimation = 0x1608;
+    uintptr_t LWProjectile_ActivateRemovedTimestamp = 0x2968;
 
-    inline uintptr_t ProjectileSpeed = 0x2484; 
-    inline uintptr_t ProjectileGravity = 0x2488; 
-    inline uintptr_t LastFireTimeVerified = 0x13A0;
-    inline uintptr_t LastFiredDirection = 0x5BC8;
-    inline uintptr_t LastFiredLocation = 0x5BB0;
+    uintptr_t ProjectileSpeed = 0x24AC; // updated: 0x29DC -> 0x24AC
+    uintptr_t ProjectileGravity = 0x24B0; // updated: 0x29E0 -> 0x24B0
+    uintptr_t MaxTargetingAimAdjustPerSecond = 0x23D0;
+    uintptr_t ServerWorldTimeSecondsDelta = 0x2E8;
+
+    uintptr_t LastFireTimeVerified = 0x142C; // added
+    uintptr_t WeaponCoreAnimation = 0x1A58; // added
 
     // ===== Items / Loot =====
-    inline uintptr_t ItemRarity = 0xAA;
-    inline uintptr_t bAlreadySearched = 0xD52;
+    uintptr_t PrimaryPickupItemEntry = 0x3A8;
+    uintptr_t ItemName = 0xB0;
+    uintptr_t ItemType = 0xA8;
+    uintptr_t PrimaryAssetOverride = 0xA9;
+    uintptr_t ItemRarity = 0xAA;
+    uintptr_t bAlreadySearched = 0xD52;
 
     // ===== Misc =====
-    inline uintptr_t HabaneroComponent = 0x948;
-    inline uintptr_t MouseSensitivityX = 0x7A8;
-    inline uintptr_t MouseSensitivityY = 0x7AC;
+    uintptr_t HabaneroComponent = 0x948;
 };
+
+// Offsets from https://github.com/paysonism/Fortnite-Offsets
+// Need more offsets? Browse the full SDK at https://fnsdk.getneptune.tech today!
