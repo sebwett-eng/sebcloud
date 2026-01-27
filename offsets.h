62873cf39eb41c27ee600ede5481a9ee3903d38a
sebwett-eng <sebwett@gmail.com>
2026-01-27 10:12:06 -0500

    AIO Multi-Offset Update

--- offsets.h
+++ offsets.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <cstdint>
+#include <string>
 
+namespace offsets {
+    inline uintptr_t GObjectsXorKey = 0x0;
+    inline uintptr_t UWorldXorKey = 0x0;
+    inline uintptr_t UWorld = 0x0;
+    inline uintptr_t GNames = 0x0;
+    inline uintptr_t GEngine = 0x0;
+    inline uintptr_t GObjects = 0x0;
+    inline uintptr_t ProcessEvent = 0x0;
+    inline uintptr_t BoneMatrix = 0x0;
+    inline std::string LastUpdate = "Tue Jan 27 10:12:05 2026";
+}
