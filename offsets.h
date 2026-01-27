d49a5f635a165507f8628d70d8d01a71863b9a73
sebwett-eng <sebwett@gmail.com>
2026-01-27 12:00:00 -0500

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -9,15 +9,15 @@ namespace offsets {
 
     // ===== Globals =====
     uintptr_t UWorld = 0x178685D8; //
-    uintptr_t UWorldXorKey = 0xFFFFFFFF30B9BBF9; // updated: 0xFFFFFFFFD599D092ULL -> 0xFFFFFFFF30B9BBF9
-    uintptr_t UWorldXorRotationCount = 0; // (simple exponent : no ror8 this update)
-    uintptr_t GNames = 0x1777E080; // updated: 0x17A50D00 -> 0x1777E080
-    uintptr_t GEngine = 0x17869ED8; // updated: 0x178C50A8 -> 0x17869ED8
-    uintptr_t GObjects = 0x1785AFB0; // updated: 0x17B2DC58 -> 0x1785AFB0
-    uintptr_t GObjectsCount = 0x1785AFB8; // updated: 0x17560ED4 -> 0x1785AFB8
+    uintptr_t UWorldXorKey = 0xFFFFFFFF30B9BBF9; 
+    uintptr_t UWorldXorRotationCount = 0; 
+    uintptr_t GNames = 0x1777E080; 
+    uintptr_t GEngine = 0x17869ED8; 
+    uintptr_t GObjects = 0x1785AFB0; 
+    uintptr_t GObjectsCount = 0x1785AFB8; 
     uintptr_t GObjectsXorKey = 0xFFFFFFFFCCF54342ULL;
     uintptr_t GObjectsCountXorKey = 0x75DB1A01;
-    uintptr_t StaticFindObject = 0x73AC13; // updated: 0x71B76C -> 0x73AC13
+    uintptr_t StaticFindObject = 0x73AC13; 
     uintptr_t StaticLoadObject = 0x960153;
 
     uintptr_t ProcessEvent = 0x86FAA; // updated: 0x63B7E -> 0x86FAA
@@ -28,7 +28,7 @@ namespace offsets {
     // ===== Player / Controller =====
     uintptr_t LocalPlayers = 0x38;
     uintptr_t PlayerController = 0x30;
-    uintptr_t PlayerCameraManager = 0x360; // updated: 0x368 -> 0x360
+    uintptr_t PlayerCameraManager = 0x360; 
     uintptr_t AcknowledgedPawn = 0x358;
     uintptr_t PlayerState = 0x2D0;
     uintptr_t NetConnection = 0x528;
@@ -47,8 +47,8 @@ namespace offsets {
     uintptr_t PlayerAimOffset = 0x2BD0;
 
     // ===== World =====
-    uintptr_t OwningGameInstance = 0x248; // updated: 0x240 -> 0x248
-    uintptr_t GameState = 0x1D0; // updated: 0x1C8 -> 0x1D0
+    uintptr_t OwningGameInstance = 0x248; 
+    uintptr_t GameState = 0x1D0; 
     uintptr_t PlayerArray = 0x2C8;
     uintptr_t WorldSettings = 0x2B8;
     uintptr_t WorldGravityZ = 0x330;
@@ -80,8 +80,8 @@ namespace offsets {
     uintptr_t LocationUnderReticle = 0x2A50;
 
     // ===== Camera =====
-    uintptr_t CameraLocation = 0x180; // updated: 0x178 -> 0x180
-    uintptr_t CameraRotation = 0x190; // updated: 0x188 -> 0x190
+    uintptr_t CameraLocation = 0x180; 
+    uintptr_t CameraRotation = 0x190; 
     uintptr_t CameraFOV = 0x3B4;
 
     // ===== Vehicles =====
@@ -89,7 +89,7 @@ namespace offsets {
 
     // ===== Weapons =====
     uintptr_t CurrentWeapon = 0x990;
-    uintptr_t WeaponData = 0x5A0; // updated: 0x600 -> 0x5A0
+    uintptr_t WeaponData = 0x5A0; 
     uintptr_t WeaponOffsetCorrection = 0x2C00;
     uintptr_t AmmoCount = 0x14FC; // updated: 0x147C -> 0x14FC
     uintptr_t bIsReloadingWeapon = 0x3D1;
