1a71bc21af72faff0e9d0fc63c6f30e8a40de5b4
sebwett-eng <sebwett@gmail.com>
2026-02-19 21:51:11 -0500

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -17,6 +17,7 @@ namespace offsets {
     inline uintptr_t RootComponent = 0x1B0;
     inline uintptr_t Spectators = 0xaa0;  
     inline uintptr_t SpectatorArray = 0x21b0;  
+    inline uintptr_t KillScore = 0x110c;
 
 
 
