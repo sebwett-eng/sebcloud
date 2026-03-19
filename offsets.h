5172514c73d3bc45a804ead47cbcfc66c5646230
sebwett-eng <sebwett@gmail.com>
2026-03-19 11:04:53 -0400

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,74 +1,66 @@
 #pragma once
 #include <cstdint>
 
-// Current Patch: v39.51 - Build 2 (Merged Update)
-// nigger
+// Current Patch: v40.00 - (Updated)
 namespace offsets {
     // world
-    inline uintptr_t UWorld = 0x17842098;
+    inline uintptr_t UWorld = 0x193717F0;
     inline uintptr_t OwningGameInstance = 0x238;
     inline uintptr_t GameState = 0x1c0;
-    inline uintptr_t PlayerArray = 0x2c8;
-    inline uintptr_t ServerWorldTimeSecondsDelta = 0x2e8; 
-    inline uintptr_t TargetedFortPawn = 0x1830; 
+    inline uintptr_t PlayerArray = 0x2C8;
+    inline uintptr_t ServerWorldTimeSecondsDelta = 0x2E8; 
+    inline uintptr_t TargetedFortPawn = 0x1810; 
     inline uintptr_t GameInstance = 0x238;
-    inline uintptr_t HabaneroRankedProgress = 0xD8;
+    inline uintptr_t HabaneroRankedProgress = 0xD8; // Matches RankedProgress
     inline uintptr_t HabaneroComponent = 0x948;
     inline uintptr_t RootComponent = 0x1B0;
-    inline uintptr_t Spectators = 0xaa0;  
-    inline uintptr_t SpectatorArray = 0x21b0;  
-    inline uintptr_t KillScore = 0x110c;
-    inline uintptr_t Reboots = 0x1108;
-    inline uintptr_t CurrentVehicle = 0x2c68;
+    inline uintptr_t Spectators = 0XAB0; // Updated from specs  
+    inline uintptr_t SpectatorArray = 0x108; // Updated from specarray 
+    inline uintptr_t KillScore = 0x11C8;
+    inline uintptr_t Reboots = 0x1894; // Updated from RebootCount
+    inline uintptr_t CurrentVehicle = 0x2CF0;
     inline uintptr_t VehicleActive = 0x813;
     inline uintptr_t VehicleGravity = 0x8A;
     inline uintptr_t RelativeLocation = 0x140;
     inline uintptr_t RotationInput = 0x428;
-    inline uintptr_t AdditionalAimOffset = 0x2b80;
-
-
-
-
-
-
+    inline uintptr_t AdditionalAimOffset = 0x2B80;
 
     inline uintptr_t PlayerName = 0xA08;
     inline uintptr_t Platform = 0x440;
     inline uintptr_t CurrentWeapon = 0x990;
-    inline uintptr_t WeaponData = 0x5B8;
+    inline uintptr_t WeaponData = 0x638;
     inline uintptr_t ItemName = 0x40;
-    inline uintptr_t Tier = 0xAA;
-
+    inline uintptr_t Tier = 0xAA; // Matches ItemRarity
 
     // local player
     inline uintptr_t LocalPlayers = 0x38;
     inline uintptr_t PlayerController = 0x30;
     inline uintptr_t AcknowledgedPawn = 0x358;
-    inline uintptr_t TeamId = 0x11B1;
+    inline uintptr_t TeamId = 0x11C1; // Updated from TeamIndex
 
     // player state
     inline uintptr_t PawnPrivate = 0x328;
-    inline uintptr_t PlayerState = 0x2D0;
-    inline uintptr_t TeamIndex = 0x11B1;
+    inline uintptr_t PlayerState = 0x2B8;
+    inline uintptr_t TeamIndex = 0x11C1;
 
     // pawn/mesh
     inline uintptr_t Mesh = 0x330;
-    inline uintptr_t BoneArray = 0x5F0;
+    inline uintptr_t BoneArray = 0x628;
     inline uintptr_t ComponentToWorld = 0x1E0;
     inline uintptr_t bIsDying = 0x728;
 
     // visibility (Found in USkinnedMeshComponent)
     inline uintptr_t bRecentlyRendered = 0x7C8;
-    inline uintptr_t LastRenderTime = 0x328;
+    inline uintptr_t LastRenderTime = 0x330;
 
     // camera
     inline uintptr_t CameraLocation = 0x170;
     inline uintptr_t CameraRotation = 0x180;
-    inline uintptr_t CameraFOV = 0x3b4;
+    inline uintptr_t CameraFOV = 0x3B4;
 
     // aimbot
     inline uintptr_t ControlRotation = 0x328;
-    inline uintptr_t PlayerAim = 0x2bb0;
+    inline uintptr_t PlayerAim = 0x29F0; // Updated from PlayerAimOffset
 
     // rarity
     inline uintptr_t Rarity = 0xAA;
@@ -79,12 +71,13 @@ namespace offsets {
     inline uintptr_t FOVMinimum = 0x2ae8;
     inline uintptr_t FOVMaximum = 0x2aec;
 
-
-    
-    // UWorld Decryption Functiom
-inline uint64_t DecryptUworld(uint64_t v)
-{
-    return ~__ROL8__(v ^ 0x97363D09LL, 50);
+    // UWorld Decryption Function
+    inline uint64_t DecryptUworld(uint64_t v)
+    {
+        // Updated with UWorldXorKey: 0x97F199673D1F48C6 and RotationCount: 32
+        return ~_rotl64(v ^ 0x97F199673D1F48C6uLL, 32);
+    }
 }
+
 
 }
